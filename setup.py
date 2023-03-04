b799537dfbcdd5f97a6e4284941f42588965a542
Sourcery AI <>
2023-03-04 10:47:50 +0000

    'Refactored by Sourcery'

--- setup.py
+++ setup.py
@@ -2,10 +2,6 @@ from sys import argv
 from setuptools import setup, find_packages
 
 
-if argv[1] in ('install', 'build', 'sdist', 'bdist_wheel'):
-  pass #will add file assoc soon  
-
- 
 classifiers = [
   'Intended Audience :: Developers',
   'Operating System :: OS Independent',
